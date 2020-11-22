7f96d1b33b4822f8daeb3b69352581f6bd69b0f4
fernandopal <f.pal.moreno@gmail.com>
2020-11-23 00:51:10 +0100

    Merged changes with master

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos32020";
+     static public $mvc_bd_usuario  = "mymontoto";
+     static public $mvc_bd_clave    = "cMirweEE";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
